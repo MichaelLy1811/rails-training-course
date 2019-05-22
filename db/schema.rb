3c076f897763285bb1a8118509f9b6121da18689
Ly <hly7@vdc.csc.com>
2019-05-22 15:31:42 +0700

    Add password column to user and password input to form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_125731) do
+ActiveRecord::Schema.define(version: 2019_05_22_080357) do
 
   create_table "members", force: :cascade do |t|
     t.string "name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_05_20_125731) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "pass"
   end
 
 end
